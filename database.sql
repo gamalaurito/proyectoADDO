f5ea5d60ddd2f167aadce6aeb1408d10d13e1407
gamalaurito <ej_sanchezg@live.com>
2017-04-27 16:46:34 -0500

    base de datos correcion

--- database.sql
+++ database.sql
@@ -1,14 +1,21 @@
+drop database jamaica;
+create database jamaica;
+
+use jamaica;
+
 create table cliente(
   cliente_num int PRIMARY KEY,
   nombre varchar(60) NOT NULL,
   email varchar(70) NOT NULL,
-  fecha_r DATE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fecha_r DATE NOT NULL,
   primera_c DATE,
   ultima_c DATE,
   num_com int NOT NULL DEFAULT 0,
   total_com FLOAT(10,3) NOT NULL DEFAULT 0.0,
   clase char(1) NOT NULL DEFAULT 'D'
 );
+
+create index email_cliente on cliente(email);
 
 create table dir_envio(
   cliente_num int,
@@ -29,13 +36,14 @@ create table producto(
   prod_num char(15) PRIMARY KEY,
   nombre varchar(20) UNIQUE NOT NULL,
   desc_p varchar(50) NOT NULL,
-  fecha_c date NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  fecha_c DATE NOT NULL,
   estatus char(1) NOT NULL DEFAULT 'A',
   image_1 BLOB NOT NULL,
   image_2 BLOB NOT NULL,
   image_3 BLOB NOT NULL,
   precio FLOAT(7,2) NOT NULL,
-  iva FLOAT(3,2) NOT NULL DEFAULT 0.16
+  iva FLOAT(3,2) NOT NULL,
+  piezas int NOT NULL DEFAULT 0
 );
 
 create table producto_info(
@@ -46,12 +54,14 @@ create table producto_info(
   FOREIGN KEY (prod_num) REFERENCES producto(prod_num)
 );
 
+create index campo_prod_info on producto_info(campo);
+
 create table categoria (
   cate_num char(10) PRIMARY KEY,
   nombre varchar(15) UNIQUE NOT NULL,
   desc_c varchar(20) NOT NULL,
   estatus char(1) NOT NULL DEFAULT 'A',
-  fecha_c date NOT NULL DEFAULT CURRENT_TIMESTAMP
+  fecha_c DATE NOT NULL
 );
 
 create table catalogo (
@@ -66,7 +76,7 @@ create table pedido_enc (
   pedido_num int PRIMARY KEY,
   cliente_num int NOT NULL,
   dir_num int NOT NULL,
-  fecha DATE NOT NULL CURRENT_TIMESTAMP,
+  fecha DATE NOT NULL ,
   pedido_clase char(1) NOT NULL DEFAULT 'N',
   observaciones varchar(20) NULL,
   importe FLOAT(10,3) NOT NULL DEFAULT 0.0,
@@ -76,13 +86,16 @@ create table pedido_enc (
   tipo_entrega char(1) NOT NULL DEFAULT 'N',
   FOREIGN KEY(cliente_num, dir_num) REFERENCES dir_envio(cliente_num, dir_num)
 );
+create index cli_dir_pedido on pedido_enc(cliente_num, dir_num);
+create index cliente_pedido on pedido_enc(cliente_num);
 
 create table pedido_det(
   pedido_num int,
   prod_num char(15),
   cantidad int,
   precio FLOAT(7,2) NOT NULL,
-  iva FLOAT(3,2) NOT NULL,
+  iva FLOAT(5,2) NOT NULL,
   PRIMARY KEY(pedido_num, prod_num),
   FOREIGN KEY(pedido_num) REFERENCES pedido_enc(pedido_num)
 );
+create index producto_pedido on pedido_det(prod_num);
